f0cb562c8859a0ef951912abe7ed999dbf1173ce
AleC77 <alessandrocalia@hotmail.com>
2021-09-20 16:18:38 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+   <Header/>
+   <Home/>
+   <Footer/>
+ </div>
+</template>
+
+<script>
+  import Header from './components/Header'
+  import Home from './components/Home'
+  import Footer from './components/Footer'
+
+  export default {
+     components:{
+         Header,
+         Home,
+         Footer
+     }
+  }
+</script>
+
+<style>
+  *{
+      box-sizing:border-box;
+      margin:0;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Home Page</h1>
+  </main>
+</template>
+
+<script>
+   export default{
+
+   }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
